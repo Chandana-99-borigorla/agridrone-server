6fc6e6dcb2bacfe6a6a2b1e91b7256852e098ca1
Borigorla  Chandana <borigorlachandana@gmail.com>
2026-06-18 18:23:18 +0530

    fix cors for production

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ dotenv.config()
 const app = express()
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://agridrone-client-git-main-chandana-99-borigorlas-projects.vercel.app'
+  ],
+  credentials: true
+}))
 app.use(express.json())
 
 // Routes
